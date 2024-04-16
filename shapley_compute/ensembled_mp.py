fddd9021f97fa9128f3a870b506183c73f15d1ae
Claire He <cmh2277@columbia.edu>
2024-04-16 13:37:52 -0400

    correction

--- shapley_compute/ensembled_mp.py
+++ shapley_compute/ensembled_mp.py
@@ -19,7 +19,7 @@ def mp_shapley(Xi, res):
     return new_shap
 
 
-def shapley_mp(target_feature, res):
+def naive_shapley_mp(target_feature, res):
     pred, in_mp_obs, in_mp_feature = res
     d = len(in_mp_feature[0])
     m = np.sum(in_mp_feature,axis=1)[0]
@@ -62,3 +62,34 @@ def shapley_mp(target_feature, res):
     shapley_j = 1/d*sum(diff)
 
     return shapley_j 
+
+
+def shapley_mp(target_feature, res):
+    pred, in_mp_obs, in_mp_loo, in_mp_feature = res
+    pred = np.array(pred)
+    n = np.sum(in_mp_obs, axis=1)[0]
+    all_features = np.unique(in_mp_feature,axis=0) # get all features sampled in MPs
+    # all_features is now one-hot encoding of the positions 
+    features_target = all_features[np.where(all_features[:,target_feature]==False)] # ohe for the feature subsets that exclude target feature
+    dk = features_target.shape[0]
+    d = all_features.shape[0]
+    
+    diff = []
+    for row in features_target[:2]:
+        val_diff = 0 
+        row_j = row.copy()
+        row_j[target_feature] = True
+    
+        # get in in_mp_feature the corresponding elements 
+        mask = np.all(in_mp_feature == row, axis=1)
+        mask_j =  np.all(in_mp_feature == row_j, axis=1)
+        if in_mp_feature[mask_j].shape[0] == 0:
+            mu_j = np.zeros(1)
+            mu_k = np.zeros(1)
+        else:
+            mu_k = np.mean(pred[mask,:,0],axis=0)
+            mu_j = np.mean(pred[mask_j,:,0], axis=0)
+        val_diff = mu_j - mu_k
+        diff.append(dk/binom(dk-1,sum(row)) * val_diff)
+    shapley_j = sum(diff)
+    return shapley_j
